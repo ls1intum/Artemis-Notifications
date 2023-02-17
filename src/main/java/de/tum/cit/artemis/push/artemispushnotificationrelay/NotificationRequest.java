7ca654b2fa4498d210077752e3d33ce1c754a7b0
Sven Andabaka <sven.andabaka@tum.de>
2023-02-17 16:54:53 +0100

    fix pr comments

--- src/main/java/de/tum/cit/artemis/push/artemispushnotificationrelay/NotificationRequest.java
+++ src/main/java/de/tum/cit/artemis/push/artemispushnotificationrelay/NotificationRequest.java
@@ -3,12 +3,11 @@ package de.tum.cit.artemis.push.artemispushnotificationrelay;
 public class NotificationRequest {
     private final String initializationVector;
     private final String payloadCipherText;
-
     private final String token;
 
     public NotificationRequest(String initializationVector, String payloadCipherText, String token) {
         this.initializationVector = initializationVector;
-        this.payloadCipherText = payloadCiphertext;
+        this.payloadCipherText = payloadCipherText;
         this.token = token;
     }
 
