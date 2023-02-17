92d67b3171c6af2f705ea449306169615e4c28e7
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2023-02-17 12:28:51 +0100

    added spring template with web.

---
+++ src/main/java/de/tum/cit/artemis/push/artemispushnotificationrelay/ArtemisPushNotificationRelayApplication.java
@@ -0,0 +1,13 @@
+package de.tum.cit.artemis.push.artemispushnotificationrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisPushNotificationRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtemisPushNotificationRelayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/tum/cit/artemis/push/artemispushnotificationrelay/ArtemisPushNotificationRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tum.cit.artemis.push.artemispushnotificationrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemisPushNotificationRelayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
