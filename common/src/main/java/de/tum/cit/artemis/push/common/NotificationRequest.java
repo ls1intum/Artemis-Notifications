ec52c8ac0c21087eac3355752c1212fda8daae17
Paul Rangger <parangger@gmail.com>
2024-11-15 14:31:49 +0100

    New iOS Notification api

--- common/src/main/java/de/tum/cit/artemis/push/common/NotificationRequest.java
+++ common/src/main/java/de/tum/cit/artemis/push/common/NotificationRequest.java
@@ -1,4 +1,4 @@
 package de.tum.cit.artemis.push.common;
 
-public record NotificationRequest(String initializationVector, String payloadCipherText, String token) {
+public record NotificationRequest(String initializationVector, String payloadCipherText, String token, PushNotificationApiType apiType) {
 }
