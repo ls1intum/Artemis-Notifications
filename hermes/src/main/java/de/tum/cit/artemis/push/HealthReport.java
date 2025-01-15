c334f8b3991c6046f0aa65ba900a4068b54193c2
Paul Rangger <parangger@gmail.com>
2025-01-15 12:16:44 +0100

    Converted Health Report to record

--- hermes/src/main/java/de/tum/cit/artemis/push/HealthReport.java
+++ hermes/src/main/java/de/tum/cit/artemis/push/HealthReport.java
@@ -1,25 +1,4 @@
 package de.tum.cit.artemis.push;
 
-public class HealthReport {
-    private final boolean isApnsConnected;
-    private final boolean isFirebaseConnected;
-    private final String versionNumber;
-
-    public HealthReport(boolean isApnsConnected, boolean isFirebaseConnected, String versionNumber) {
-        this.isApnsConnected = isApnsConnected;
-        this.isFirebaseConnected = isFirebaseConnected;
-        this.versionNumber = versionNumber;
-    }
-
-    public boolean isApnsConnected() {
-        return isApnsConnected;
-    }
-
-    public boolean isFirebaseConnected() {
-        return isFirebaseConnected;
-    }
-
-    public String getVersionNumber() {
-        return versionNumber;
-    }
+public record HealthReport(boolean isApnsConnected, boolean isFirebaseConnected, String versionNumber) {
 }
